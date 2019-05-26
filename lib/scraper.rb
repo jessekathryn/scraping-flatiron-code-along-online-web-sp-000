21c8d77dc1e89b0736dd9c1c8e442609a55e761f
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-05-26 21:53:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-
-require_relative './course.rb'
-
-class Scraper
-    
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
  
 require_relative './course.rb'
  
